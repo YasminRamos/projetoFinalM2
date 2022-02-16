b0d9c79d410b469b399a0c64f7a4b5a66794385a
Yasmin Ramos <yasmincoelhoramos@gmail.com>
2022-02-16 12:52:14 -0300

    pagina de cadastro modificada

---
+++ projetoFinal.js
@@ -0,0 +1,25 @@
+//validação de e-mail
+$(document).ready(function() {
+	$("p").hide();
+	$('#verificar-email').click(function(){
+		//desabilitando o submit do form
+		$("form").submit(function () { return false; });
+		//atribuindo o valor do campo
+		var sEmail	= $("#email").val();
+		// filtros
+		var emailFilter=/^.+@.+\..{2,}$/;
+		var illegalChars= /[\(\)\<\>\,\;\:\\\/\"\[\]]/
+		// condição
+		if(!(emailFilter.test(sEmail))||sEmail.match(illegalChars)){
+			$("p").show().removeClass("ok").addClass("erro")
+			.text('Por favor, informe um email válido.');
+		}else{
+			$("p").show().addClass("ok")
+			.text('Email informado está correto!');
+		}
+	});
+	$('#email').focus(function(){
+		$("p.erro").hide();
+	});
+});	
+
